929d3256b703175610082ad52fac0e108544115f
Luke Van Bergen <lukepvb@gmail.com>
2021-01-21 13:59:56 -0800

    Initial main image in, moving on to energy savings component

--- octopus_energy/client/containers/BodyContainer.jsx
+++ octopus_energy/client/containers/BodyContainer.jsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
-import NavBar from '../components/NavBar';
+import React from 'react';
+import MainImage from '../components/MainImage';
 import '../assets/styles.css';
 
 const BodyContainer = (props) => {
   return (
     <div className="body-container">
-      <h1>body component 1</h1>
-      <h1>body component 2</h1>
-      <h1>body component 3</h1>
+      <MainImage />
     </div>
   );
 };
--- octopus_energy/webpack.config.js
+++ octopus_energy/webpack.config.js
@@ -61,6 +61,6 @@ module.exports = {
   },
   resolve: {
     // Enable importing JS / JSX files without specifying their extension
-    extensions: ['.js', '.jsx', '.css'],
+    extensions: ['.js', '.jsx', '.css', '.jpg'],
   },
 };
