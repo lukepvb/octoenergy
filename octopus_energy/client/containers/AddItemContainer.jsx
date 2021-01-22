2fc492f4173e25f791c5030e29077b88b8bd2253
Luke Van Bergen <lukepvb@gmail.com>
2021-01-22 10:21:16 -0800

    quantity counter updated and functional, moving on to specifications and description modals

--- octopus_energy/client/containers/AddItemContainer.jsx
+++ octopus_energy/client/containers/AddItemContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from '../components/Item';
 import PriceAndQuantity from '../components/PriceAndQuantity';
 import QuantityCounter from '../components/QuantityCounter';
@@ -6,11 +6,30 @@ import AddToCart from '../components/AddToCart';
 import '../assets/styles.css';
 
 const AddItemContainer = (props) => {
+  const [count, setCount] = useState(1);
+
+  const increment = (value) => {
+    setCount(value + 1);
+  };
+
+  const decrement = (value) => {
+    if (value > 1) {
+      setCount(value - 1);
+    }
+  };
+
+  const handleClick = (str, curCount) => {
+    if (str === 'increment') {
+      increment(curCount);
+    } else if (str === 'decrement') {
+      decrement(curCount);
+    }
+  };
   return (
     <div className="add-item-container">
       <Item />
       <PriceAndQuantity />
-      <QuantityCounter />
+      <QuantityCounter count={count} handleClick={handleClick} />
       <AddToCart />
     </div>
   );
